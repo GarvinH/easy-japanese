378ba316966217b2c7385b3d5d47a067772ee34d
GarvinH <garvinh2001@hotmail.com>
2021-07-12 22:47:30 -0400

    Adjust generalizability of article section

--- components/ArticleSection.tsx
+++ components/ArticleSection.tsx
@@ -2,40 +2,48 @@ import React from "react";
 import { View, StyleSheet } from "react-native";
 import { Paragraph, Title } from "react-native-paper";
 import YoutubePlayer from "react-native-youtube-iframe";
-import _ from "lodash"
+import _ from "lodash";
 
 import dim from "../constants/Layout";
 import globalStyles from "../constants/Styles";
 
-import Anchor, { AnchorProps } from "./Anchor"
+import Anchor, { AnchorProps } from "./Anchor";
 
 export interface ArticleSectionProps {
-    title: string,
-    description: string,
-    videoId: string,
-    externalArticles: AnchorProps[],
+  title: string;
+  description: string;
+  videoId?: string;
+  externalArticles?: AnchorProps[];
 }
 
-const ArticleSection = ({title, description, videoId, externalArticles}: ArticleSectionProps) => (
+const ArticleSection = ({
+  title,
+  description,
+  videoId,
+  externalArticles,
+}: ArticleSectionProps) => (
   <View>
     <Title style={styles.title}>{title}</Title>
-    <Paragraph>
-      {description}
-    </Paragraph>
-    <View style={styles.subsection}>
-      <Title>Videos</Title>
-      <YoutubePlayer
-        videoId={videoId}
-        height={
-          ((dim.window.width - 2 * globalStyles.container.padding) * 9) / 16
-        }
-      />
-    </View>
-    <View style={{...styles.subsection, alignItems: "baseline"}}>
-      <Title>Articles</Title>
-      {_.map(externalArticles, (article) => <Anchor text={article.text} href={article.href}/>)}
-      
-    </View>
+    <Paragraph>{description}</Paragraph>
+    {videoId && (
+      <View style={styles.subsection}>
+        <Title>Videos</Title>
+        <YoutubePlayer
+          videoId={videoId}
+          height={
+            ((dim.window.width - 2 * globalStyles.container.padding) * 9) / 16
+          }
+        />
+      </View>
+    )}
+    {externalArticles && (
+      <View style={{ ...styles.subsection, alignItems: "baseline" }}>
+        <Title>Articles</Title>
+        {_.map(externalArticles, (article) => (
+          <Anchor text={article.text} href={article.href} />
+        ))}
+      </View>
+    )}
   </View>
 );
 
